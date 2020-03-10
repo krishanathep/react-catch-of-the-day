02610c5a5340eed325db88696a3e52958bec8f70
krishanathep <krishanathep@gmail.com>
2020-03-10 16:18:00 +0700

    Updates

---
+++ precache-manifest.e7a8fe6425085c9c53da513905e5b2f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202aea582da752935ef53ba7f2c1a099",
+    "url": "/react-catch-of-the-day/index.html"
+  },
+  {
+    "revision": "5a4daeae7a9666826959",
+    "url": "/react-catch-of-the-day/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "191fb1bae76e2401d692",
+    "url": "/react-catch-of-the-day/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5a4daeae7a9666826959",
+    "url": "/react-catch-of-the-day/static/js/2.75eeb660.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-catch-of-the-day/static/js/2.75eeb660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "191fb1bae76e2401d692",
+    "url": "/react-catch-of-the-day/static/js/main.ca62a0d6.chunk.js"
+  },
+  {
+    "revision": "98a4f7e202235c4b5565",
+    "url": "/react-catch-of-the-day/static/js/runtime-main.5f60858c.js"
+  }
+]);
